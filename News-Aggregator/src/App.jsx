13bdde2b82cea4c155119f6cf8d98435a78341c3
itsNamanmishra <naman.mishra_cs22@gla.ac.in>
2025-03-27 10:21:35 +0530

    First Commit

--- News-Aggregator/src/App.jsx
+++ News-Aggregator/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import {NewsHeader} from './components/JSX/NewsHeader'
-
+import { NewsFooter } from './components/JSX/NewsFooter'
+import { NewsHome } from './components/JSX/NewsHome'
+import './App.css'
 const App = () => {
   return (
     <div>
       <NewsHeader/>
+      <NewsHome/>
+      <NewsFooter/>
     </div>
   )
 }
