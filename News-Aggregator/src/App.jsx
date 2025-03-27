dd634265153a595378047db8aa47e5cf26e57997
Kunal1767 <143917451+Kunal1767@users.noreply.github.com>
2025-03-27 10:55:58 +0530

    #

--- News-Aggregator/src/App.jsx
+++ News-Aggregator/src/App.jsx
@@ -1,15 +1,11 @@
 
 import React from 'react'
-import {NewsHeader} from './components/JSX/NewsHeader'
-import { NewsFooter } from './components/JSX/NewsFooter'
-import { NewsHome } from './components/JSX/NewsHome'
+
 import './App.css'
 const App = () => {
   return (
     <div>
-      <NewsHeader/>
-      <NewsHome/>
-      <NewsFooter/>
+      
     </div>
   )
 }
